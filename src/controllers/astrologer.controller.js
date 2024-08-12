e72333711307fc1b6d3f8adcf2e89a2ffda024cc
Abani-kumar <abanikumar073@gmail.com>
2024-08-13 01:09:36 +0530

    updated

--- src/controllers/astrologer.controller.js
+++ src/controllers/astrologer.controller.js
@@ -66,30 +66,37 @@ export const getAllAstrologers = async (req, res) => {
     }
 
     const skip = limit * (page - 1);
+
+    const query = {};
+    const conditions = [];
+
     if (req.query.language) {
       const languages = req.query.language
         .split("+")
         .map(
           (lang) => lang.charAt(0).toUpperCase() + lang.slice(1).toLowerCase()
         );
-
       if (languages.length > 0) {
-        query.$or.push({ language: { $in: languages } });
+        conditions.push({ language: { $in: languages } });
       }
     }
 
     if (req.query.expertise) {
       const expertises = req.query.expertise
         .split("+")
-        .map(expert => expert.charAt(0).toUpperCase() + expert.slice(1).toLowerCase());
-
+        .map(
+          (expert) =>
+            expert.charAt(0).toUpperCase() + expert.slice(1).toLowerCase()
+        );
       if (expertises.length > 0) {
-        query.$or.push({ expertise: { $in: expertises } });
+        conditions.push({ expertise: { $in: expertises } });
       }
     }
-    if (query.$or.length === 0) {
-      query.$or.push({});
+
+    if (conditions.length > 0) {
+      query.$and = conditions;
     }
+
     console.log("Query:", JSON.stringify(query));
     const astrologers = await Astrologer.find(query, { description: 0 })
       .sort(sortOption)
